90f27c2ff33d17b9b440f10e78fe72c5db12d898
Iuliia Shevtsova <uliyaal2018@gmail.com>
2020-11-23 21:57:15 -0500

    add view, create, update, delete Comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
+  resources :posts 
 
   # get '/pages', to: 'pages#index'
   # post '/pages', to: 'pages#create'
@@ -8,9 +8,24 @@ Rails.application.routes.draw do
   # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
   # patch '/pages/:id', to: 'pages#update'
   # delete '/pages/:id', to: 'pages#destroy'
-
-  # We can replace all of the above with a single line
+  # --- We can replace all of the above with a single line
   resources :pages
+
+
+  # resources :posts do
+  #   get    '/comments',          to: 'comments#index', as: 'comments'
+  #   post   '/comments',          to: 'comments#create'
+  #   get    '/comments/new',      to: 'comments#new',   as: 'new_comment'
+  #   get    '/comments/:id',      to: 'comments#show',  as: 'comment'
+  #   get    '/comments/:id/edit', to: 'comments#edit',  as: 'edit_comment'
+  #   patch  '/comments/:id',      to: 'comments#update'
+  #   delete '/comments/:id',      to: 'comments#destroy'
+  # end
+ # ---- We can replace all of the above with a block
+resources :posts do
+  resources :comments
+end
+
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
